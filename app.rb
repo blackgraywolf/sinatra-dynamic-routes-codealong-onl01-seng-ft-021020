377a67f5543ec1a1fb8e031fe4c05a082c5d2fef
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-28 19:08:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,18 +1,64 @@
 require_relative 'config/environment'
 
+# class App < Sinatra::Base
+
+#   # This is a sample static route.
+#   get '/hello' do
+#     "Hello World!"
+#   end
+
+#   # This is a sample dynamic route.
+#   get "/hello/:name" do
+#     @user_name = params[:name]
+#     "Hello #{@user_name}!"
+#   end
+
+#   # Code your final two routes here:
+
+# end
+
 class App < Sinatra::Base
-
-  # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
+	
+  get '/reversename/:name' do
+    @reverse_name = params[:name].reverse
+    @reverse_name
   end
 
-  # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+  get '/square/:number' do
+    @num = params[:number].to_i ** 2
+    @num.to_s
   end
 
-  # Code your final two routes here:
+  get '/say/:number/:phrase' do
+    str = ""
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
+  end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @oper = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case @oper
+      when "subtract"
+        (@num1 - @num2).to_s
+      when "add"
+        (@num1 + @num2).to_s
+      when "multiply"
+        (@num1 * @num2).to_s
+      when "divide"
+        (@num1 / @num2).to_s
+    end
+  end
 end
